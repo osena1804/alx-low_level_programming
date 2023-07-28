48fcc9f17dde8d913e77b672b8466f14b62e991a
osena1804 <osenalinda@gmail.com>
2023-07-28 03:08:37 -0700

    0-print_list.c

--- 0-print_list.c
+++ 0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *head)
 
 	while (head)
 	{
-		if (!head->str)
+		if (head->str != NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", head->len, head->str);
