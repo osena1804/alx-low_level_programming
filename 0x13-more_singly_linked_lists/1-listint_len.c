ddd157fda6d44ea896a4a7647fb596242cdb5c3e
osena1804 <osenalinda@gmail.com>
2023-08-01 18:01:03 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,10 +5,10 @@
 
 /**
  * listint_len -  returns the number of elements in a linked listint_t lists
- * @h : traverse linked lists of type listint_t
- *@num : return value
  *
- * Return : number of nodes in the linked lists
+ * @h : traverse linked lists of type listint_t
+ *
+ * Return: returns number of nodes in the linked lists
  */
 
 size_t listint_len(const listint_t *h)
